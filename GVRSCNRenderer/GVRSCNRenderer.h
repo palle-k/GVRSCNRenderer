605df571595f5067321b5133b0ba19bc11d304c4
Palle Klewitz <palle.klewitz@tum.de>
2017-12-25 15:53:07 +0100

    Initial Commit

---
+++ GVRSCNRenderer/GVRSCNRenderer.h
@@ -0,0 +1,19 @@
+//
+//  GVRSCNRenderer.h
+//  GVRSCNRenderer
+//
+//  Created by Palle Klewitz on 25.12.17.
+//  Copyright © 2017 Palle Klewitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GVRSCNRenderer.
+FOUNDATION_EXPORT double GVRSCNRendererVersionNumber;
+
+//! Project version string for GVRSCNRenderer.
+FOUNDATION_EXPORT const unsigned char GVRSCNRendererVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GVRSCNRenderer/PublicHeader.h>
+
+
